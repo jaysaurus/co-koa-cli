83fed361d91279ca302e186511a58d3a579f9a52
Jaysaurus <the.jaysaurus@gmail.com>
2017-12-09 22:12:56 +0000

    - refactored YargOptions.js to stampit convention - refactored Component.js to stampit convention - decoupled some logic from Component.js into ComponentHelper.js - refined component program exit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 'use strict';
+
 const echoHandler = require('echo-handler').configure({ messageFolder: `${__dirname}/lib/i18n` });
 
 const CLIAction = require('./lib/CLIAction');
 const YargOptions = require('./lib/YargOptions');
-const options = new YargOptions(echoHandler.load('yargOptions')).describe();
+const options = YargOptions(echoHandler.load('yargOptions')).describe();
 
 let yargs = require('yargs')
   .options(options)
@@ -22,7 +23,6 @@ try {
     if (!actionTaken) actionTaken = cliAction.parse(key);
     else break;
   }
-
   if (!actionTaken) echo.throw('noArguments');
 } catch (e) {
   console.error(e.message);
