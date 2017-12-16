3f44fc04cb8f7b2f3ef0f910eeb5dd85ac25adf1
Jaysaurus <the.jaysaurus@gmail.com>
2017-12-16 18:16:28 +0000

    - ComponentHelper.js reformatted a line - Coverage written for ComponentHelper.js

--- __mocks__/fs-extra.js
+++ __mocks__/fs-extra.js
@@ -5,31 +5,31 @@ var observer = {
   ensureDirSync: [],
   outputFileSync: [],
   readFileSync: []
-}
+};
 
 fs.copySync = function (from, to) {
   observer['copySync'].push({ from, to });
-}
+};
 
 fs.ensureDirSync = function (dir) {
   observer['ensureDirSync'].push(dir);
-}
+};
 
 fs.outputFileSync = function (file, template) {
   observer['outputFileSync'].push(file);
   observer['outputFileSync'].push(template);
-}
+};
 
-var data = ""
+var data = '';
 
 fs.__setData = function (dat) {
   data = dat;
-}
+};
 
 fs.readFileSync = function (dir) {
   observer['readFileSync'].push(dir);
   return data;
-}
+};
 
 fs.__resetObserver = function (spy) {
   observer = Object.assign({}, {
@@ -38,10 +38,10 @@ fs.__resetObserver = function (spy) {
     outputFileSync: [],
     readFileSync: []
   });
-}
+};
 
 fs.__getObserver = function (type) {
   return observer[type];
-}
+};
 
 module.exports = fs;
--- __tests__/ComponentHelper.test.js
+++ __tests__/ComponentHelper.test.js
@@ -1,1 +1,17 @@
-const lib = require('../lib/helpers/ComponentHelper.js');
+const ComponentHelper = require('../lib/helpers/ComponentHelper.js');
+
+jest.mock('fs-extra');
+describe('ComponentHelper tests', () => {
+  test('parseTemplate reads through each line of the given co.koa template', () => {
+    const fs = require('../__mocks__/fs-extra.js');
+    fs.__setData('#name#\n#name#\nand a test line: #name#\n');
+    const result = ComponentHelper('testDirectory').parseTemplate(
+      'example',
+      'test');
+    const observer = fs.__getObserver('readFileSync');
+    expect(observer[0]).toBe('testDirectory/example.co.koa');
+    expect(result)
+      .toBe('test\ntest\nand a test line: test\n');
+
+  });
+});
