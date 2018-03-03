15b257404f1e26b813c3c1404744b17e7bfe8651
Jaysaurus <the.jaysaurus@gmail.com>
2018-03-03 17:45:15 +0000

    - AssetConfig.js amended in line with better setup for vue js - removed public folder legacy folders - removed references to legacy public folders from projectHelper.js - added script calls to package.json

--- dist/config/AssetConfig.js
+++ dist/config/AssetConfig.js
@@ -1,20 +1,24 @@
+'use strict';
+
 module.exports = ['development', 'test', 'production'].reduce(
   (obj, environment) => {
     switch (environment) {
       case 'development':
       case 'test':
-        return Object.assign(obj, { [environment]: {
-          'css': '/css',
-          'html': '/html',
-          'img': '/images',
-          'js': '/js'
-        } });
+        obj[environment] = {
+          'css': '/src/assets/css',
+          'html': '/',
+          'img': '/src/assets/img',
+          'js': '/src/assets/js'
+        };
+        break;
       case 'production':
-        return Object.assign(obj, { [environment]: {
-          'css': '/.min/css',
-          'html': '/html',
-          'img': '/images',
-          'js': '/.min/js'
-        } });
+        obj[environment] = {
+          'css': '/static/css',
+          'html': '/',
+          'img': '/static/img',
+          'js': '/static/js'
+        };
     }
+    return obj;
   }, {});
