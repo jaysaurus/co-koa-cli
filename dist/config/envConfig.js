183203297d7e38a5b5caaecf830fbbec2ca1b54a
Jaysaurus <the.jaysaurus@gmail.com>
2018-03-12 23:35:54 +0000

    changes to installable distribution to bring it in line with co-koa-core 1.2.0

--- dist/config/envConfig.js
+++ dist/config/envConfig.js
@@ -2,11 +2,13 @@ module.exports = (env, envConfig, mongoose) => {
   switch (env) {
     case 'development':
     case 'test':
-      mongoose.Promise = global.Promise;
-      mongoose
-        .connect(envConfig['mongoDB_URI'])
-        .catch(e => { console.error(e.message); });
-      envConfig.mongoose = mongoose;
+      if (mongoose) {
+        mongoose.Promise = global.Promise;
+        mongoose
+          .connect(envConfig['mongoDB_URI'])
+          .catch(e => { console.error(e.message); });
+        envConfig.mongoose = mongoose;
+      }
       return envConfig;
 
     case process.env.NODE_ENV:
